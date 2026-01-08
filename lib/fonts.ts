ded0b4c0e9f82b520788dbdb693d0444c3a49a04
Sandhyaravoor <sandhyaravoor.25@gmail.com>
2026-01-08 16:48:31 +0530

    update

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,27 +3,27 @@ import localFont from 'next/font/local';
 export const parabolica = localFont({
   src: [
     {
-      path: '../public/fonts/parabolica-light.woff2',
+      path: '../public/fonts/Parabolica-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/parabolica-regular.woff2',
+      path: '../public/fonts/Parabolica-Regular.woff',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/parabolica-medium.woff2',
+      path: '../public/fonts/Parabolica-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/parabolica-bold.woff2',
+      path: '../public/fonts/Parabolica-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/parabolica-black.woff2',
+      path: '../public/fonts/Parabolica-Black.woff2',
       weight: '900',
       style: 'normal',
     },
