e503f79c8b433841a71f0b8467028bb9b2ba4247
BizTradeFairs <biztradefairs.com@gmail.com>
2026-01-19 17:55:32 +0530

    update

--- components/section/MapSection.tsx
+++ components/section/MapSection.tsx
@@ -12,15 +12,14 @@ const MapSection = () => {
             {/* Venue */}
             <div className="bg-blue-50 p-8 rounded-lg">
               <h3 className="text-lg lg:text-xl font-semibold text-blue-600 mb-4">Venue</h3>
-              <p className="text-gray-800 font-medium text-lg">Rosaski, Moscov, Crocus Expo IEC, Pavilion 3</p>
+              <p className="text-gray-800 font-medium text-lg">Auto Cluster Exhibitoin Centre, Pune, India</p>
             </div>
 
             {/* Opening Hours */}
             <div className="bg-blue-50 p-8 rounded-lg">
               <h3 className="text-lg lg:text-xl font-semibold text-blue-600 mb-4">Opening Hours</h3>
-              <p className="text-gray-800 font-medium text-lg">12-16 March 2026, 10:00 -18:00</p>
-              <p className="text-gray-800 font-medium text-lg">19 March 2026, 10:00 -18:00</p>
-            </div>
+              <p className="text-gray-800 font-medium text-lg">08-10 October 2026, 10:00 -18:00</p>
+                          </div>
           </div>
 
           {/* Map */}
@@ -29,7 +28,7 @@ const MapSection = () => {
               width="100%"
               height="100%"
               frameBorder="0"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.8743484199996!2d37.51654!3d55.61139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b53a9e6c8c1111%3A0x1234567890ab!2sCrocus%20Expo%2C%20Moscow!5e0!3m2!1sen!2sru"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4651.861093715675!2d73.79904587592934!3d18.638844465549226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b84992d04bbd%3A0x9f1c44fb853ba461!2sAuto%20Cluster%20Exhibition%20Center%2C%20Chinchwad%2C%20Pune!5e1!3m2!1sen!2sin!4v1768825009655!5m2!1sen!2sin"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
