ad2ab8231b4a3e179b76b8a5bd3fe78289f2e01b
biztradefairs <biztradefairs.com@gmail.com>
2026-02-21 22:58:42 +0530

    Update conference organizer description

--- app/conference/components/OrganizerSection.tsx
+++ app/conference/components/OrganizerSection.tsx
@@ -7,7 +7,7 @@ export default function OrganizerSection() {
           <div className="rte-style [&_a]:underline [&_blockquote]:relative [&_blockquote]:ml-5 [&_blockquote]:w-fit [&_blockquote]:border-l-4 [&_blockquote]:border-black [&_blockquote]:bg-[#f9f9f9] [&_blockquote]:p-5 [&_blockquote]:italic [&_h1]:lg:text-4xl [&_h2]:lg:text-3xl [&_h3]:lg:text-2xl [&_ol]:list-decimal [&_ol]:pl-5 [&_ul]:list-disc [&_ul]:pl-5">
             <p className="mb-4">
               <span className="text-base md:text-lg lg:text-[27px]">
-                TransRussia Summit is held under the auspices of the largest{' '}
+               The DIEMEX Conference is held alongside DIEMEX – International Exhibition for Die & Mould, Tooling, and Precision Manufacturing Technologies, one of India’s focused platforms for the tooling and precision engineering industry.{' '}
                 <a href="https://transrussia.ru/ru/">
                   <span className="text-gray-700">
                     international exhibition of transport and logistics services TransRussia
