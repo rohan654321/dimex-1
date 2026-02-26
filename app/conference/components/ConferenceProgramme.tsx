854832e8bc6d18e2027c04eed18fd21f03c5916a
BizTradeFairs <biztradefairs.com@gmail.com>
2026-02-26 16:06:24 +0530

    update

--- app/conference/components/ConferenceProgramme.tsx
+++ app/conference/components/ConferenceProgramme.tsx
@@ -74,7 +74,7 @@ const TransRussiaSummitProgram: React.FC = () => {
                    
           speakers: [
         {
-              name: "TBD",
+              name: "",
               position: "",
             },
               ],
@@ -85,7 +85,7 @@ const TransRussiaSummitProgram: React.FC = () => {
                    
           speakers: [
         {
-              name: "TBD",
+              name: "",
               position: "",
             },
               ],
@@ -97,7 +97,7 @@ const TransRussiaSummitProgram: React.FC = () => {
                    
           speakers: [
         {
-              name: "TBD",
+              name: "",
               position: "",
             },
               ],
