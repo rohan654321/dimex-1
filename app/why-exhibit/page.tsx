cf73ccc5d9d5d93cbdbdc114a2050d460896c4e2
BizTradeFairs <biztradefairs.com@gmail.com>
2026-03-04 20:07:33 +0530

    update

--- app/why-exhibit/page.tsx
+++ app/why-exhibit/page.tsx
@@ -738,7 +738,7 @@ export default function WhyExhibit() {
   className="relative font-parabolica py-16 lg:py-24 text-white bg-cover bg-center"
   style={{
     backgroundImage:
-      "url('https://cdn.itegroupnews.com/Frame_1707480883_b10f284c3d.jpg')",
+      "url('/images/why-exhibit-header.JPG')",
   }}
 >
   {/* Dark overlay */}
