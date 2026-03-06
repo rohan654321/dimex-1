6abebdf46935a8823efc178921e0dd4dd0847442
BizTradeFairs <biztradefairs.com@gmail.com>
2026-03-06 17:50:59 +0530

    update

--- app/culture/components/HeroSection.tsx
+++ app/culture/components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection: React.FC = () => {
     <section className="relative min-h-[60vh] lg:min-h-[70vh] z-10 flex flex-col justify-end bg-gray-100 pt-96">
       <div className="container mx-auto flex flex-col justify-end pb-10 text-white">
         <h2 className="text-6xl md:text-7xl lg:text-8xl font-bold text-white mb-4">
-          Immerse in Moscow's Culture
+          Immerse in Pune's Culture
         </h2>
         <p className="max-w-6xl whitespace-pre-line py-5"></p>
       </div>
@@ -13,8 +13,8 @@ const HeroSection: React.FC = () => {
       <div className="absolute inset-0 z-[-1] w-full h-full">
         <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
         <img 
-          src="https://cdn.itegroupnews.com/Untitled_design_47_330a961bb8.png" 
-          alt="Moscow Culture" 
+          src="/images/image.png" 
+          alt="Pune" 
           className="w-full h-full object-cover"
         />
       </div>
