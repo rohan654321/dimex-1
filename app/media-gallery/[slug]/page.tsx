59866a056ba4501e85f4a29739b53690e6c55962
rohan654321 <mondalrohan201@gmail.com>
2026-03-07 16:21:37 +0530

    update

--- app/media-gallery/[slug]/page.tsx
+++ app/media-gallery/[slug]/page.tsx
@@ -73,7 +73,7 @@ const mediaDetails = [
     title: "Diemex 2023 Inauguration",
     shortText: "Diemex2023 Inauguration Ceremony",
     images: [
-      { src: "/images/media-gallery/2023IN_1.JPG", alt: "Diemex 2023 Inauguration" },
+      { src: "/images/media-gallery/20205IN_29.JPG", alt: "Diemex 2023 Inauguration" },
       // Add more 2023 images based on your actual files
     ]
   },
@@ -83,7 +83,7 @@ const mediaDetails = [
     title: "Diemex 2023 Exhibition",
     shortText: "Diemex2023 Exhibition Highlights",
     images: [
-      { src: "/images/media-gallery/2023EX_1.JPG", alt: "Diemex 2023 Exhibition" },
+      { src: "/images/media-gallery/20205IN_27.JPG", alt: "Diemex 2023 Exhibition" },
       // Add more 2023 images based on your actual files
     ]
   },
@@ -93,7 +93,7 @@ const mediaDetails = [
     title: "Diemex 2023 Awards Ceremony",
     shortText: "Diemex2023 Awards Ceremony",
     images: [
-      { src: "/images/media-gallery/2023AW_1.JPG", alt: "Diemex 2023 Awards" },
+      { src: "/images/media-gallery/20205IN_27.JPG", alt: "Diemex 2023 Awards" },
       // Add more 2023 images based on your actual files
     ]
   }
