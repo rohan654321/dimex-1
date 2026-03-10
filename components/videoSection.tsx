d07801eedddd4ca707f83ded5df9ecc62a04497f
BizTradeFairs <biztradefairs.com@gmail.com>
2026-03-10 20:59:23 +0530

    update

--- components/videoSection.tsx
+++ components/videoSection.tsx
@@ -2,7 +2,7 @@ import SectionContainer from "./UI/SectionContainer"
 
 export default function VideoSection() {
   // --- VIDEO ID FROM YOUR LINK: https://www.youtube.com/watch?v=wJcae3DLXc0 ---
-  const VIDEO_ID = "3P-hRFrsXIs";
+  const VIDEO_ID = "3hz69OrvXHY";
   // ---
 
   const embedUrl = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&mute=1&playsinline=1&rel=0&modestbranding=1&controls=0&loop=1&playlist=${VIDEO_ID}`;
@@ -24,7 +24,7 @@ export default function VideoSection() {
           <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
             <div className="bg-[#2BB7A8]/90 px-10 py-4 rounded-md">
               <span className="text-white text-2xl md:text-4xl font-bold tracking-wide">
-                DIEMEX 2025 HIGHLIGHTS
+                DIEMEX 2023 HIGHLIGHTS
               </span>
             </div>
           </div>
