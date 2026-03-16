0aefa385ee483537a1e55597e7b0e95f93054e88
BizTradeFairs <biztradefairs.com@gmail.com>
2026-03-16 15:58:46 +0530

    update

--- app/free-promo/page.tsx
+++ app/free-promo/page.tsx
@@ -268,7 +268,7 @@ export default function ExhibitorPromotionPage() {
 
                 {/* BUTTON */}
                 <a
-                  href="/images/exhibitor-promo/Diemex_2026_logos"
+                  href="/images/exhibitor-promo/Diemex_2026_logos.zip"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mt-6 inline-flex w-fit rounded-full bg-[#004D9F] px-12 py-4 text-lg font-semibold text-white hover:bg-mainColor4 transition"
