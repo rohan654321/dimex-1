aec857b1eea4124a3d9d457286e863889271bfc5
rohan654321 <mondalrohan201@gmail.com>
2026-03-16 11:41:27 +0530

    update

--- app/exhibitor-resource-center/component/ContactInformation.tsx
+++ app/exhibitor-resource-center/component/ContactInformation.tsx
@@ -2,57 +2,43 @@ import SectionContainer from '@/components/UI/SectionContainer';
 
 export default function ContactInformation() {
   return (
-    <section className="py-16 bg-white">
-      <SectionContainer>
-        <h2 className="title-72 text-black mb-12">
-          Contact Information
-        </h2>
+    <div>
+      <h2 className="title-72 text-black mb-8">
+        Contact Information
+      </h2>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-          {/* Left Card */}
-          
-          {/* Right Card */}
-          <div className="rounded-2xl bg-[#F3F9FC] p-8 flex flex-col justify-between">
-            <div>
-              <p className="text-lg leading-relaxed text-black/80 mb-6">
-                For stall design approvals of RAW Space Exhibitors, please
-                contact our  manager:
-              </p>
+      <div className="rounded-2xl bg-[#F3F9FC] p-8 shadow-sm max-w-md">
+        <p className="text-lg leading-relaxed text-black/80 mb-6">
+          For stall design approvals of RAW Space Exhibitors, please
+          contact our manager:
+        </p>
 
-              <p className="text-xl font-semibold text-black">
-                Satish
-              </p>
+        <p className="text-xl font-semibold text-black">
+          Satish
+        </p>
 
-              <p className="text-base text-black/70 mb-5">
-                Technical Manager              </p>
+        <p className="text-base text-black/70 mb-5">
+          Technical Manager
+        </p>
 
-              <p className="text-lg mb-3">
-                <a
-                  href="mailto:Dmitry.Shishanov@ite.group"
-                  className="underline hover:text-mainColor2"
-                >
-                  info@diemex.in
-                </a>
-              </p>
+        <p className="text-lg mb-3">
+          <a
+            href="mailto:info@diemex.in"
+            className="underline hover:text-mainColor2"
+          >
+            info@diemex.in
+          </a>
+        </p>
 
-              <p className="text-lg">
-                <a
-                  href="tel:+74957995585"
-                  className="underline text-[#4D4D4D]"
-                >
-                  +91 63649 36468
-                </a>
-              </p>
-            </div>
-
-            <img
-              src=""
-              alt=""
-              className="mt-6 w-32 rounded-md object-cover"
-            />
-          </div>
-        </div>
-      </SectionContainer>
-    </section>
+        <p className="text-lg">
+          <a
+            href="tel:+916364936468"
+            className="underline text-[#4D4D4D]"
+          >
+            +91 63649 36468
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
--- app/exhibitor-resource-center/page.tsx
+++ app/exhibitor-resource-center/page.tsx
@@ -1,18 +1,11 @@
-
 import BackToTop from './component/BackToTop';
 import HeroSection from './component/HeroSection';
 import ExploreOpportunities from './component/ExploreOpportunities';
 import BusinessObjectives from './component/BusinessObjectives';
 import SelectionMadeSimple from './component/SelectionMadeSimple';
 import LogisticsMarketing from './component/LogisticsMarketing';
-import TechnicalServices from './component/TechnicalServices';
-import CeilingSuspension from './component/CeilingSuspension';
-import ApplicationDeadline from './component/ApplicationDeadline';
 import ContactInformation from './component/ContactInformation';
 import AttentionSection from './component/AttentionSection';
-// import PartnersSection from '@/components/section/PartnersSection';
-
-
 
 export default function ExhibitorResourceCenterPage() {
   return (
@@ -23,14 +16,16 @@ export default function ExhibitorResourceCenterPage() {
         <BusinessObjectives />
         <SelectionMadeSimple />
         <LogisticsMarketing />
-        {/* <TechnicalServices /> */}
-        {/* <CeilingSuspension /> */}
-        {/* <ApplicationDeadline /> */}
-        <ContactInformation />
-        <AttentionSection />
-        {/* <PartnersSection /> */}
+
+        <section className="py-16 bg-white">
+          <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
+            <ContactInformation />
+            <AttentionSection />
+          </div>
+        </section>
+
       </main>
-    
+
       <BackToTop />
     </>
   );
